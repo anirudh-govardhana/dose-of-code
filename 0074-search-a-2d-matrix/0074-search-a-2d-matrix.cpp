8128eef7f0cb4484c793c30fa465b147e409584d
Anirudh Govardhana <anirudhgovardhana4@gmail.com>
2026-06-05 18:26:23 +0530

    Time: 0 ms (100%), Space: 13.4 MB (6.99%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-
-    bool binSearch(vector<int> &m, int st, int end, int target){
-        while(st<=end){
-            int mid = st+(end-st)/2;
-            if(m[mid]==target) return true;
-            else if(m[mid]>target) end = mid-1;
-            else st = mid+1;
-        }
-        return false;
-    }
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
+        int st=0, end=n*m-1;
 
-        for(int i=0;i<m;i++){
-            if(matrix[i][0] <= target && target <= matrix[i][n-1]){
-                return binSearch(matrix[i], 0, n-1, target);
-            }
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            int val = matrix[mid/n][mid%n];
+            if(val==target) return true;
+            else if(val>target) end = mid-1;
+            else st = mid+1;
         }
         return false;
     }
